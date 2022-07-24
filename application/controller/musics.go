c832b3d0870e8500a70f869b0c29113cfa882a79
devjoeween <jjazan201@gmail.com>
2022-07-25 00:38:12 +0900

    Youtube search
    - Add youtube client
    	- #15
    	- #16
    	- #17
    	- #20
    - Added middlewares
    	- `etag` middleware
    	- `cors` middleware
      - restore middleware
    - Added custom Error handler
    - Added swagger
    - #45
    
    - filter youtube playlist (set videoDuration only more than 20m)
    
    <img width="974" alt="Screen Shot 2022-07-30 at 10 33 01 PM" src="https://user-images.githubusercontent.com/37536298/181916776-3c3e8661-03d4-4778-b8d8-6c05c42b8f28.png">
    
    > http://127.0.0.1:8080/api/v1/musics/search?order=count&q=상쾌,일본
    
    ```
    {
      "code": 0,
      "data": [
        {
          "youtubeVideoID": "hz27Smhnv7I",
          "thumbnailURL": "https://i.ytimg.com/vi/hz27Smhnv7I/default.jpg",
          "title": "[Playlist] 안질리는 유명한 J-POP 모음│YOASOBI, ZUTOMAYO, 요루시카, 카미야마 요우, Eve",
          "youtubeTags": [
            "우타이테",
            "일본노래",
            "유명한일본노래"
          ],
          "videoDeepLink": "vnd.youtube://hz27Smhnv7I",
          "isMemoed": false
        },
        {
          "youtubeVideoID": "wGf1-8b8fZo",
          "thumbnailURL": "https://i.ytimg.com/vi/wGf1-8b8fZo/default.jpg",
          "title": "[ PLAYLIST ] #26 상쾌하고✨기분 좋아지는 일본 노래 (가사, 번역) | Cheerful Japanese Songs (lyrics) | 明るい歌 (歌詞)",
          "youtubeTags": [
            "아이묭",
            "슈퍼플라이",
            "수퍼플라이",
            "레이야스다",
            "야스다레이",
            "음악",
            "음악추천",
            "노래",
            "노래추천",
            "일본",
            "일본노래",
            "제이팝",
            "밤",
            "늦은밤",
            "밤늦게",
            "감성",
            "감성적인노래",
            "락음악",
            "락",
            "팝",
            "시티팝",
            "플레이리스트",
            "노래리스트",
            "LIST",
            "playlist",
            "emotional",
            "emotion",
            "music",
            "song",
            "lyrics",
            "가사",
            "번역",
            "통역",
            "통번역",
            "jpop",
            "japan",
            "japanese",
            "愛を伝えたいだとか",
            "Ai Wo Tsutaetaidatoka",
            "あいみょん",
            "Aimyon",
            "Rei Yasuda",
            "Ohara Sakurako",
            "Little Glee Monster",
            "Superfly",
            "Shishamo"
          ],
          "videoDeepLink": "vnd.youtube://wGf1-8b8fZo",
          "isMemoed": false
        },
        {
          "youtubeVideoID": "d5jMy9nF-3A",
          "thumbnailURL": "https://i.ytimg.com/vi/d5jMy9nF-3A/default.jpg",
          "title": "청량 치사량 🐬 일본노래 모음 [jpop playlist]",
          "youtubeTags": [
            "playlist",
            "플레이리스트",
            "jpop",
            "일본노래",
            "청량"
          ],
          "videoDeepLink": "vnd.youtube://d5jMy9nF-3A",
          "isMemoed": false
        },
    	... 중략 ...
      ]
    }
    ```

---
+++ application/controller/musics.go
@@ -0,0 +1,99 @@
+package controller
+
+import (
+	"github.com/Nexters/myply/domain/entity"
+	"github.com/Nexters/myply/domain/service"
+	"github.com/gofiber/fiber/v2"
+	"go.uber.org/zap"
+)
+
+type MusicController interface {
+	Search() fiber.Handler // TODO: pagination
+}
+
+type musicController struct {
+	logger       *zap.SugaredLogger
+	musicService service.MusicsService
+}
+
+func NewMusicController(l *zap.SugaredLogger, ms service.MusicsService) MusicController {
+	return &musicController{logger: l, musicService: ms}
+}
+
+const (
+	RecentOrder  = "recent"
+	PopularOrder = "count"
+)
+
+type SearchQueryParams struct {
+	Q         []string `query:"q"`
+	Order     string   `query:"order"`
+	NextToken string   `query:"token"`
+}
+
+type MusicResponse struct {
+	YoutubeVideoID string   `json:"youtubeVideoID"`
+	ThumbnailURL   string   `json:"thumbnailURL"`
+	Title          string   `json:"title"`
+	YoutubeTags    []string `json:"youtubeTags"`
+	VideoDeepLink  string   `json:"videoDeepLink"`
+	IsMemoed       bool     `json:"isMemoed"`
+}
+
+type ListMusicResponse struct {
+	BaseResponse
+	Data []MusicResponse `json:"data"`
+}
+
+// @Summary      Search music playlist
+// @Description  플레이리스트 검색
+// @Tags         musics
+// @Accept       json
+// @Produce      json
+// @Param payload query SearchQueryParams true "query params"
+// @Success      200  {object}   ListMusicResponse
+// @Failure      500
+// @Router       /musics/search [get]
+func (mc *musicController) Search() fiber.Handler {
+
+	return func(ctx *fiber.Ctx) error {
+		var (
+			musics *entity.Musics
+			err    error
+		)
+		p := new(SearchQueryParams)
+		err = ctx.QueryParser(p)
+		if err != nil {
+			return fiber.NewError(fiber.StatusBadRequest, err.Error())
+		}
+
+		switch p.Order {
+		case RecentOrder:
+			mc.logger.Info("Recent order")
+		case PopularOrder:
+			musics, err = mc.musicService.GetPopularList(p.Q)
+		default:
+			return fiber.NewError(fiber.StatusInternalServerError, "Unknown order")
+
+		}
+		if err != nil {
+			return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+		}
+
+		data := []MusicResponse{}
+		for _, m := range *musics {
+			data = append(data, MusicResponse{
+				YoutubeVideoID: m.YoutubeVideoID,
+				ThumbnailURL:   m.ThumbnailURL,
+				Title:          m.Title,
+				YoutubeTags:    m.YoutubeTags,
+				VideoDeepLink:  m.DeepLink(),
+				IsMemoed:       false, // TODO: memo service
+			})
+		}
+		return ctx.Status(200).JSON(BaseResponse{
+			Code: Ok,
+			Data: data,
+		})
+	}
+}
--- application/wire_gen.go
+++ application/wire_gen.go
@@ -7,21 +7,22 @@
 package application
 
 import (
-	"context"
 	"fmt"
+	"github.com/Nexters/myply/application/controller"
+	"github.com/Nexters/myply/application/router"
+	"github.com/Nexters/myply/docs"
+	"github.com/Nexters/myply/domain/service"
+	"github.com/Nexters/myply/infrastructure/clients"
 	"github.com/Nexters/myply/infrastructure/configs"
 	"github.com/Nexters/myply/infrastructure/logger"
 	"github.com/Nexters/myply/infrastructure/persistence"
 	"github.com/Nexters/myply/infrastructure/persistence/db"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/etag"
+	recover2 "github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/gofiber/swagger"
-	"github.com/google/uuid"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.uber.org/zap"
-)
-
-import (
-	_ "github.com/Nexters/myply/docs"
 )
 
 // Injectors from server.go:
@@ -39,48 +40,77 @@ func New() (*fiber.App, error) {
 	if err != nil {
 		return nil, err
 	}
-	app := NewServer(config, sugaredLogger, mongoInstance)
+	youtubeClient, err := clients.NewYoutubeClient(config)
+	if err != nil {
+		return nil, err
+	}
+	musicRepository := persistence.NewMusicRepository()
+	musicsService := service.NewMusicService(sugaredLogger, youtubeClient, musicRepository)
+	musicController := controller.NewMusicController(sugaredLogger, musicsService)
+	routerRouter := router.NewMusicsRouter(musicController)
+	app := NewServer(config, sugaredLogger, mongoInstance, routerRouter)
 	return app, nil
 }
 
 // server.go:
 
-// @title MYPLY SERVER
-// @version 1.0
-// @description This is a sample swagger for Fiber
-// @termsOfService http://swagger.io/terms/
-// @contact.name API Support
-// @contact.email minkj1992@gmail.com
-// @license.name Apache 2.0
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-// @host localhost:8080
-// @BasePath /
-func NewServer(config *configs.Config, logger2 *zap.SugaredLogger, mongo *db.MongoInstance) *fiber.App {
+func NewServer(
+	config *configs.Config, logger2 *zap.SugaredLogger,
+	mongo *db.MongoInstance,
+	musicsRouter router.MusicsRouter,
+) *fiber.App {
+	app := fiber.New(fiber.Config{
 
-	collection := mongo.Db.Collection("members")
-	member := persistence.Member{
-		ID:      uuid.NewString(),
-		Name:    "leoo",
-		MemoIDs: []primitive.ObjectID{},
-	}
+		ErrorHandler: CustomErrorHandler,
+	})
 
-	insertionResult, _ := collection.InsertOne(context.Background(), member)
-	logger2.
-		Infof("Instance\n%+v", insertionResult)
-	logger2.
-		Infof("Configuration settings\n%+v", config)
-	app := fiber.New()
+	app.Use(cors.New())
+	app.Use(etag.New())
+	app.Use(recover2.New())
 
+	setSwagger(config.BaseURI)
 	app.Get("/swagger/*", swagger.HandlerDefault)
-	app.Get("/swagger/*", swagger.New(swagger.Config{
-		URL:         "http://example.com/doc.json",
-		DeepLinking: false,
 
-		DocExpansion: "none",
-	}))
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString(fmt.Sprintf("[%s] Hello, myply ✈️", config.Phase))
 	})
 
+	api := app.Group("/api")
+	v1 := api.Group("/v1")
+
+	musicsRouter.Init(&v1)
+
 	return app
+}
+
+func setSwagger(baseURI string) {
+	docs.SwaggerInfo.
+		Title = "Myply Server ✈️"
+	docs.SwaggerInfo.
+		Description = "This is a My Playlist server."
+	docs.SwaggerInfo.
+		Version = "1.0"
+	docs.SwaggerInfo.
+		Host = baseURI
+	docs.SwaggerInfo.
+		BasePath = "/api/v1"
+}
+
+func CustomErrorHandler(ctx *fiber.Ctx, err error) error {
+
+	httpCode := fiber.StatusInternalServerError
+	code := controller.Unknown
+	message := "Internal Server Error"
+
+	if e, ok := err.(*fiber.Error); ok {
+		httpCode = e.Code
+		message = e.Message
+
+	}
+
+	ctx.Set(fiber.HeaderContentType, fiber.MIMETextPlainCharsetUTF8)
+	return ctx.Status(httpCode).JSON(controller.BaseResponse{
+		Code:    code,
+		Message: message,
+	})
 }
